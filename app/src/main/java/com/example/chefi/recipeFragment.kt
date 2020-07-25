8acfd020ba2295a00a1098883d0994c3c0a9de52
chagayh <chagay.hof@gmail.com>
2020-07-26 00:57:58 +0300

    added navigation btw favorites and recipe framents

--- app/src/main/java/com/example/chefi/recipeFragment.kt
+++ app/src/main/java/com/example/chefi/recipeFragment.kt
@@ -5,10 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FavoritesFragment.newInstance] factory method to
+ * Use the [recipeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FavoritesFragment : Fragment() {
+class recipeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,13 +33,8 @@ class FavoritesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_favorites, container, false)
-        val btn = view.findViewById<Button>(R.id.button)
-        btn.setOnClickListener { v ->
-            v.findNavController().navigate(R.id.recipeFragment)
-        }
         // Inflate the layout for this fragment
-        return view
+        return inflater.inflate(R.layout.fragment_recipe, container, false)
     }
 
     companion object {
@@ -53,12 +44,12 @@ class FavoritesFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoritesFragment.
+         * @return A new instance of fragment recipeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FavoritesFragment().apply {
+            recipeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
